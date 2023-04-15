b227d673865f1390e39194262d6b516ac5f04544
Kazuma.I <51313744+Kazuma7@users.noreply.github.com>
2023-04-16 04:18:43 +0900

    Fix: Airstackの設定

--- front/pages/_app.tsx
+++ front/pages/_app.tsx
@@ -1,6 +1,8 @@
 import { GlobalProvider } from "../contexts/Global";
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
+// import { ApolloClient, InMemoryCache, gql } from "@apollo/client/core";
+import { ApolloClient, ApolloProvider, InMemoryCache } from "@apollo/client";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
