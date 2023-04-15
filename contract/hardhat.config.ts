bfdf561baca23f8741eda2e0e6a637458fd070fc
taxio <taxio.2029@gmail.com>
2023-04-15 16:57:24 +0900

    setup test

--- contract/hardhat.config.ts
+++ contract/hardhat.config.ts
@@ -2,7 +2,28 @@ import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
 
 const config: HardhatUserConfig = {
-  solidity: "0.8.18",
+  solidity: {
+    version: "0.8.18",
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 200,
+      },
+    },
+  },
+  etherscan: {
+    apiKey: process.env.SCAN_API_KEY,
+  },
+  gasReporter: {
+    enabled: !!process.env.REPORT_GAS,
+    currency: "USD",
+    token: process.env.GAS_REPORTER_TOKEN,
+    coinmarketcap: process.env.COIN_MARKET_CAP_API_KEY,
+  },
+  defaultNetwork: "hardhat",
+  networks: {
+    hardhat: {}
+  }
 };
 
 export default config;
