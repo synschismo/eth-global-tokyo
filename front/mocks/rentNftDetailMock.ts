f5b867c0141f011d3fb30a6f9120a818c9ccb020
Kazuma.I <51313744+Kazuma7@users.noreply.github.com>
2023-04-16 02:23:56 +0900

    Fix: 修正

--- front/mocks/rentNftDetailMock.ts
+++ front/mocks/rentNftDetailMock.ts
@@ -3,7 +3,7 @@ import { RentNftDetailType } from "../types/RentNftDetailType";
 export const rentNftDetailMock: RentNftDetailType = {
   collectionName: "VeryLongAnimals",
   name: "VeryLongAnimals #1",
-  image: "/sample8.png",
+  image: "/sample3.png",
   price: "0.01",
   chainId: "1",
   status: "available",
